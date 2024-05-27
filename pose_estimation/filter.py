5e2b1b498dcb49514d8bff051014238c71fb33ac
riordk626 <riordk@stanford.edu>
2024-05-27 00:12:45 -0700

    further fleshed out filter class file

--- pose_estimation/filter.py
+++ pose_estimation/filter.py
@@ -30,17 +30,22 @@ class Filter:
 class MEKF(Filter):
     def __init__(self, mu0, Sig0, qref, 
                  dynFunc,
+                 kinFunc,
+                 quatReset,
                  measFunc,
                  ssMatfunc = ssDef,
                  dt = 1,
                  rng_seed = 273):
         super().__init__(mu0, Sig0, dynFunc, measFunc, ssMatfunc, dt, rng_seed)
         self.qref = qref
+        self.kinFunc = kinFunc
+        self.quatReset = quatReset
 
     def step(self, u, y):
         A, B, C, Q, R = self.ssMatFunc(self.mu, u, self.dt)
 
         # predict step
+        q_tplus_t = self.kinFunc(self.mu, self.qref)
         mu_tplus_t = self.dynFunc(self.mu, u, A, B, self.dt)
         Sig_tplus_t = A @ self.Sig @ A.T + Q
 
@@ -48,10 +53,15 @@ class MEKF(Filter):
         K = Sig_tplus_t @ C.T @ np.linalg.inv(C @ Sig_tplus_t @ C.T + R)
         z = self.measFunc(mu_tplus_t, C, self.dt)
 
-        self.mu = mu_tplus_t + K @ (y - z)
+        mu_tplus_tplus = mu_tplus_t + K @ (y - z)
         self.Sig = Sig_tplus_t - K @ C @ Sig_tplus_t
 
         # reset step
+        self.qref = self.quatReset(mu_tplus_tplus, q_tplus_t)
+        self.mu = np.block([
+            [np.zeros([3, 1])],
+            [mu_tplus_t[3:]]
+        ])
 
         return self.mu, self.Sig
     
