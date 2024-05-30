d65604e0eff733535117e847b38ac7a2de75d2a2
GigaVoltFlash <anshukc@gmail.com>
2024-05-30 00:24:33 -0700

    Changes to integration file

--- integrated_run.py
+++ integrated_run.py
@@ -4,17 +4,25 @@ from pose_estimation.visual_odometry import VisualOdometry, load_original_traj
 import numpy as np
 
 if __name__ == "__main__":
+    # Load original traj
     traj_path = ...
     true_positions, true_attitudes = load_original_traj(traj_path)
+
     n = len(true_positions)
+
+    # Set up image generator
     image_creator = ImageGenerator()
+
+    # Set up visual odometry
     marker_side_len = 10.0
     marker_orig = np.array([0.0, 0.0, 2.5])
     K_mtx = np.array([[424.3378, 0., 424.], [0., 424.3378, 240.], [0., 0., 1.]])
     dist_coeffs = np.zeros((4, 1))
     vision = VisualOdometry(marker_side_len, marker_orig, dist_coeffs, K_mtx)
-
-    folder_path = './blender_sim/simulation_imgs'
+    
+    
     
     for i in range(n):
         # do stuff
+        # For every image
+        folder_path = './blender_sim/simulation_imgs'
